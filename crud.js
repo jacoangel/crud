6886b9e390b6251a0a021896fbafba2a23e908d5
jacoangel <jacoangel98@gmail.com>
2021-06-23 16:07:12 -0500

    arreglando problema primer intento

--- crud.js
+++ crud.js
@@ -1,5 +1,11 @@
 // array que contiene los datos
-let cars = []
+let cars = [ {
+  name: "Mazda 2",
+  model: "2019",
+  doors: 5,
+  color: "red",
+  brand: "mazda"
+}]
 
     //   una variable guarda html
 let   carsListCont =  document.getElementById("carsList")
